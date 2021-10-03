878af8c0cd46276d7a6cebd25a8e870f0b63153c
Ammar Khaled Haddad <ammar96haddad@gmail.com>
2021-10-03 19:42:48 +0200

    a

--- UiA-bacit-skeleton/build.cmd
+++
@@ -1,3 +0,0 @@
-docker run --rm -it --name mavenbuild -v maven-repo:/root/.m2 -v "%cd%":/usr/src/mymaven -w /usr/src/mymaven maven mvn clean install
-docker image build -t trym/tomcat .
-docker container run --rm -it --publish 8081:8080 trym/tomcat
--- UiA-bacit-skeleton/startDb.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm --name mariadb -p 3308:3306/tcp -v "$(pwd)/database":/var/lib/mysql -e MYSQL_ROOT_PASSWORD=12345 -d mariadb
